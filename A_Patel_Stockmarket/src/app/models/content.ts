fc302d8fa6a373a4babeec9c22a744b07335f90b
abhi0565 <91274937+abhi0565@users.noreply.github.com>
2022-05-15 23:01:59 -0500

    Update content.ts
    update the content

--- A_Patel_Stockmarket/src/app/models/content.ts
+++ A_Patel_Stockmarket/src/app/models/content.ts
@@ -1,2 +1,13 @@
+import { Optional } from "@angular/core";
+
 export interface Content {
+    id: number;
+    title : string;
+    body? : string;
+    author : string;
+    imagelink? : string;
+    type : string[];
+
+
 }
+
