047ac669d99957213cc6d8e7bd0bd3905cb5332d
abhi0565 <91274937+abhi0565@users.noreply.github.com>
2022-05-17 20:58:52 -0500

    list

--- A_Patel_Stockmarket/src/app/app.component.ts
+++ A_Patel_Stockmarket/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { List } from './models/list';
 
 @Component({
   selector: 'app-root',
@@ -8,3 +9,10 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'A_Patel_Stockmarket';
 }
+
+
+
+
+
+
+
---
+++ A_Patel_Stockmarket/src/app/models/list.ts
@@ -0,0 +1,23 @@
+import { Content } from "@angular/compiler/src/render3/r3_ast";
+
+
+export class List {
+  static ContentCount = 0;
+  private _items: Content[];
+  constructor(item: Content) {
+    this._items = []; // initialize array
+    this._items[0] = item; // set the value of the item at the first index of the array
+   this.increaseCount();
+  }
+  get items():Content[] {
+    return this._items;
+  }
+  increaseCount() {
+
+    return ++List.ContentCount;
+
+  }
+
+
+
+}
