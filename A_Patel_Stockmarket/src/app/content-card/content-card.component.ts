c64c4a050ff9536e96a42a362f6f66008d901d9a
abhi0565 <91274937+abhi0565@users.noreply.github.com>
2022-05-23 20:26:33 -0500

    style from app-component

--- A_Patel_Stockmarket/src/app/content-card/content-card.component.ts
+++ A_Patel_Stockmarket/src/app/content-card/content-card.component.ts
@@ -4,7 +4,7 @@ import { Content } from '../models/content';
 @Component({
   selector: 'app-content-card',
   templateUrl: './content-card.component.html',
-  styleUrls: ['./content-card.component.scss']
+  styleUrls: ['../app.component.scss']
 })
 export class ContentCardComponent implements OnInit {
   @Input() contentItem?: Content;
