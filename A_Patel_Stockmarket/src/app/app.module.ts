34a8b4a3ed2d1da273e543afa55d7c3ed4e371c5
abhi0565 <91274937+abhi0565@users.noreply.github.com>
2022-05-28 19:50:06 -0500

    error solving

--- A_Patel_Stockmarket/src/app/app.module.ts
+++ A_Patel_Stockmarket/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContentListComponent } from './content-list/content-list.component';
 import { ContentCardComponent } from './content-card/content-card.component';
-import { StocksPipe } from '../../pipes/stocks.pipe';
+import { StocksPipe } from './stocks.pipe';
 
 @NgModule({
   declarations: [
