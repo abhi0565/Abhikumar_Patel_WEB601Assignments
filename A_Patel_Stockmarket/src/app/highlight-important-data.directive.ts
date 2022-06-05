8f88e4a1679a07e76e286d35dc7e4ad8a748cc0c
abhi0565 <91274937+abhi0565@users.noreply.github.com>
2022-06-05 16:07:29 -0500

    added border to type

--- A_Patel_Stockmarket/src/app/highlight-important-data.directive.ts
+++ A_Patel_Stockmarket/src/app/highlight-important-data.directive.ts
@@ -1,10 +1,27 @@
-import { Directive } from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[appHighlightImportantData]'
 })
 export class HighlightImportantDataDirective {
 
-  constructor() { }
-
+  @Input() defaultColor:string="";  
+  @Input() highlightColor:string="";  
+   
+@HostBinding('style.border') border:string=this.defaultColor;  
+ 
+  constructor( private eleRef:ElementRef){}  
+  
+  @HostListener('mouseover') mouseover(eventData:Event){  
+      this.border=this.highlightColor;  
+     // this.eleRef.nativeElement.style.color="White";  
+  }  
+ 
+  @HostListener('mouseleave') mouseleave(eventData:Event){  
+      this.border=this.defaultColor;  
+      //this.eleRef.nativeElement.style.color="Black";  
+  } 
 }
+
+  
+
