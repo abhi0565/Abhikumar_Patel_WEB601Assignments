8347056162b569535eacda82352d7171c2b337a7
abhi0565 <91274937+abhi0565@users.noreply.github.com>
2022-06-05 22:58:17 -0500

    added style to the title

--- A_Patel_Stockmarket/src/app/highlight-important-data.directive.ts
+++ A_Patel_Stockmarket/src/app/highlight-important-data.directive.ts
@@ -5,22 +5,73 @@ import {Directive, ElementRef, HostBinding, HostListener, Input } from '@angular
 })
 export class HighlightImportantDataDirective {
 
-  @Input() defaultColor:string="";  
+  private defaultColor:string="";  
   @Input() highlightColor:string="";  
+  @Input()color:string="";
+
+
+  private isHighlighted: boolean = false;
+  private initialColour: string;
+ // private initialTextColor: string;
+
+
+
+  @HostBinding('style.backgroundColor')
+  get backgroundColour() {
+    return this.isHighlighted ? this.color || "" :
+      this.initialColour;
+  }
+  @HostBinding('style.color')
+  get textColour() {
+    return this.isHighlighted;
+  }
+
+
+  
+  constructor(private elm: ElementRef) {
+    this.initialColour = this.elm.nativeElement.style.backgroundColor;
+
+  }
+
+  @HostListener('click') onClick() {
+    this.isHighlighted = !this.isHighlighted;
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
    
 @HostBinding('style.border') border:string=this.defaultColor;  
  
-  constructor( private eleRef:ElementRef){}  
+  // constructor( private eleRef:ElementRef){}  
   
   @HostListener('mouseover') mouseover(eventData:Event){  
       this.border=this.highlightColor;  
-     // this.eleRef.nativeElement.style.color="White";  
+  
   }  
  
   @HostListener('mouseleave') mouseleave(eventData:Event){  
       this.border=this.defaultColor;  
       //this.eleRef.nativeElement.style.color="Black";  
   } 
+
+
+
+
+
+
+
 }
 
   
