6e948bcebf90ff669bbcda880d17496a8a09adda
abhi0565 <91274937+abhi0565@users.noreply.github.com>
2022-06-10 18:31:12 -0500

    change the location of  data array to the data folder

--- A_Patel_Stockmarket/src/app/content-list/content-list.component.ts
+++ A_Patel_Stockmarket/src/app/content-list/content-list.component.ts
@@ -12,65 +12,7 @@ export class ContentListComponent implements OnInit {
   stockdata:Content[];
   public  inputvalue?: Optional;
   constructor() { 
-    this.stockdata = [{
-      id: 0,
-      title: 'TATA MOTORS',
-      body: "automobile",
-      author: "Ratan Tata",
-     // imagelink: "https://m.economictimes.com/thumb/msid-91550659,width-1200,height-900,resizemode-4,imgsize-56418/tata-motors.jpg",
-      type: "auto",
-      hashtags: ["tata", "auto", "nano"]
-    },{
-      id: 1,
-      title: 'Relience',
-      body: "all sector brand",
-      author: "Mukesh Ambani",
-      imagelink: "https://englishtribuneimages.blob.core.windows.net/gallary-content/2022/5/2022_5$largeimg_2129243640.jpg",
-      type: "all",
-      hashtags: ["Ambani"]
-    },{
-      id: 2,
-      title: 'Adani',
-      body: "new billionaire",
-      author: "Gautam Adani",
-      imagelink: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9sSw_P016iDbR7n0GPrZpFzRTUYp3jWDBdA&usqp=CAU",
-      type: "all",
-      hashtags: ["Adani"]
-    },{
-      id: 3,
-      title: 'HDFC',
-      body: "HDFC",
-      author: "abhi",
-      imagelink: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4WNTgpKO7eK2BzuuXKmG7we8qy5rY5lA6VQ&usqp=CAU",
-      type: "bank",
-      hashtags: ["HDFC"]
-    },{
-      id: 4,
-      title: 'SBI',
-      body: "SBI",
-      author: "state bank of india",
-      imagelink: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOEP2Udx6t1uC_8MnekJVH62yLE9oGEyFPQg&usqp=CAU",
-      type: "bank",
-      hashtags: ["SBI"]
-    },{
-      id: 5,
-      title: 'ITC',
-      body: "ITC",
-      author: "indian tobbaco company",
-      imagelink: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/ITC_Limited_Logo.svg/1200px-ITC_Limited_Logo.svg.png",
-      type: "FMCG",
-      hashtags: ["ITC"]
-    },{
-      id: 6,
-      title: 'D-Mart',
-      body: "D-mart",
-      author: "Radha krishna damani",
-      imagelink: "https://m.economictimes.com/thumb/msid-69749642,width-1200,height-900,resizemode-4,imgsize-363172/7.jpg",
-     // type: "FMCG",
-      hashtags: ["D-Mart"]
-    }
-    ]
-    this.inputvalue ="";
+         this.stockdata=[];
   }
 
   ngOnInit(): void {
