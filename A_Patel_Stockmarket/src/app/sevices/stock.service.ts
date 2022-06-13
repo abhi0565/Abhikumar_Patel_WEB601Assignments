adcb259705c664f85bb9ed835cc92ca98e7f207b
abhi0565 <91274937+abhi0565@users.noreply.github.com>
2022-06-13 11:14:31 -0500

    crud operation

--- A_Patel_Stockmarket/src/app/sevices/stock.service.ts
+++ A_Patel_Stockmarket/src/app/sevices/stock.service.ts
@@ -1,9 +1,68 @@
 import { Injectable } from '@angular/core';
+import { Content } from '../models/content';
+import { Observable, of } from 'rxjs';
+import { stockdata } from '../data/mock-stock';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StockService {
 
-  constructor() { }
+  constructor() { 
+  }
+
+// retun stockdata array
+  getContent(): Observable<Content[]> {
+    return of(stockdata);
+  }
+// searching in stockdata array
+   getContentItem(id: number): Observable<Content> {
+    for (var i = 0; i < stockdata.length; i++) {
+      if (stockdata[i].id === id) {
+        return of(stockdata[i]);
+      }
+    }
+   }
+
+//creat a new item to the array
+   addContentItem(newContent: Content): Observable<Content[]> {
+        let a = {
+
+          id: 7,
+          title: 'abc',
+          body: "abc",
+          author: "abc",
+          type: "abc",
+          hashtags: ["abc"]
+
+        }
+        stockdata.push(a)
+      return of(stockdata);
+   }
+
+      updateContentItem(newContent: Content): Observable<Content[]> {
+      let  Update = {id: 7, title:'xyz'};
+      stockdata.forEach(item =>{
+        if( item.id == Update.id){
+            item.title = Update.title;
+        }
+    });
+      return of(stockdata);
+  }
+ 
+  deleteContentItem(newContent: Content): Observable<Content[]> {
+  
+    function RemoveElementFromArray(key: number) {
+      stockdata.forEach((value,index)=>{
+          if(value.id == key)stockdata.splice(index,1);
+  
+      });
+  }
+  return of(stockdata);
+  }
+
+
+
+
+
 }
