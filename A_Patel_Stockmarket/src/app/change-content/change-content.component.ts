fdda0ea6a01a9e7098ab8c60b753c0ecd57c7947
abhi0565 <91274937+abhi0565@users.noreply.github.com>
2022-07-10 21:47:11 -0500

    added add content to the list

--- A_Patel_Stockmarket/src/app/change-content/change-content.component.ts
+++ A_Patel_Stockmarket/src/app/change-content/change-content.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Content } from '../models/content';
+import { StockService } from '../sevices/stock.service';
 
 @Component({
   selector: 'app-change-content',
@@ -6,10 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./change-content.component.scss']
 })
 export class ChangeContentComponent implements OnInit {
-
-  constructor() { }
+  contentItem: Content = {
+    title: "",
+    body: "",
+    id: null,
+    author: '',
+    type:'',
+    hashtags: []
+  };
+  tempTags: string = '';
+  constructor(private StockService: StockService) { }
 
   ngOnInit(): void {
+  }
+
+  addContentToServer(): void {
+    this.contentItem.hashtags = this.tempTags.split(", ");
+    this.StockService.addContentItem(this.contentItem)
+      .subscribe(newContentFromServer =>
+        console.log("Success! New content added", newContentFromServer)
+      );
+  }
+  updateContentOnServer(): void {
+    this.contentItem.hashtags = this.tempTags.split(", ");
+    this.StockService.updateContentItem(this.contentItem)
+      .subscribe(() =>
+        console.log("Content updated successfully", this.contentItem)
+      );
   }
 
 }
