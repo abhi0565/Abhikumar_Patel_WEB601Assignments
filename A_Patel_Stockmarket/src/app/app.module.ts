888e7f2a3ba141318c28c5c20b17d3b378473beb
abhi0565 <91274937+abhi0565@users.noreply.github.com>
2022-07-24 15:52:06 -0500

    added custome angular material

--- A_Patel_Stockmarket/src/app/app.module.ts
+++ A_Patel_Stockmarket/src/app/app.module.ts
@@ -15,6 +15,7 @@ import { HttpClientModule } from "@angular/common/http";
 import { InMemoryDataService } from './services/in-memory-data.service';
 import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
 import { ChangeContentComponent } from './change-content/change-content.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
@@ -39,6 +40,7 @@ import { ChangeContentComponent } from './change-content/change-content.componen
       InMemoryDataService, {
       delay: 1000,
     }),
+    BrowserAnimationsModule,
 
   ],
   providers: [],
